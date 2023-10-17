907ce026a01d15b4f5e62f8aab803668b53d3fb8
Matheus Costa Rodrigues <88292487+Masaki-007@users.noreply.github.com>
2023-10-17 15:16:52 -0300

    Add files via upload

---
+++ controller/SplashScreen.java
@@ -0,0 +1,23 @@
+package controller;
+
+public class SplashScreen {
+    public static void showSplashScreen() {
+        System.out.println("--------------------------------------------");
+        System.out.println("#           SISTEMA DE RH                  #");
+        System.out.println("#                                          #");
+        System.out.println("#   REGISTROS:                             #");
+        System.out.println("#   REGISTRO 1:                            #");
+        System.out.println("#   REGISTRO 2:                            #");
+        System.out.println("#                                          #");
+        System.out.println("#   DESENVOLVIDO POR:                      #");
+        System.out.println("#   JOÃO ANTONIO CARNEIRO VASCONCELOS      #");
+        System.out.println("#   GABRIEL SALOMÃO PANCIONE               #");
+        System.out.println("#   MATHEUS COSTA RODRIGUES                #");
+        System.out.println("#                                          #");
+        System.out.println("#   DISCIPLINA: BANCO DE DADOS             #");
+        System.out.println("#   2023/2                                 #");
+        System.out.println("#                                          #");
+        System.out.println("#   PROFESSOR: HOWARD ROATTI               #");
+        System.out.println("--------------------------------------------");
+    }
+}
---
+++ model/ConexaoBanco.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBanco {
+    private String host = "Localhost";
+    private String database = "XE";
+    private String user = "labdatabase";
+    private String password = "labDatabase2022";
+    private int port = 1521;
+    private Connection conn = null;
+
+    public ConexaoBanco() {
+        conectar();
+    }
+
+    private void conectar() {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            String url = "jdbc:oracle:thin:@" + host + ":" + port + ":" + database;
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Erro ao carregar o driver JDBC do Oracle: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar ao banco de dados Oracle: " + e.getMessage());
+        }
+    }
+
+    public Connection getConexao() {
+        return conn;
+    }
+
+    public void close() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao fechar a conexão: " + e.getMessage());
+        }
+    }
+}
+
